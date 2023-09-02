0cb3b404c80540c0b3e6836d78ff5afb7aab5f58
bektilekdev <sbektilek@gmail.com>
2023-09-02 18:15:19 +0600

    jn

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import './home.scss';
+import React from "react";
+import "./home.scss";
+import HomePopular from "../../Page/HomePopular";
 const Home = () => {
-    return (
-        <div id="home">
-            <div className='home'>
-                <div className='home_nav'>
-                    <h1>Добро пожаловать</h1>
-                    <p>Миллионы фильмов, сериалов и людей. Исследуйте сейчас.</p>
-                    <input type="text" placeholder='Найти фильмь,сериал,персону...'/>
-                    <button>Search</button>
-                </div>
-            </div>
+    
+  return (
+    <div id="home">
+      <div className="home">
+        <div className="home_nav">
+          <h1>Добро пожаловать</h1>
+          <p>Миллионы фильмов, сериалов и людей. Исследуйте сейчас.</p>
+          <input type="text" placeholder="Найти фильмь,сериал,персону..." />
+          <button>Search</button>
         </div>
-    );
+      </div>
+      <HomePopular/>
+    </div>
+  );
 };
 
 export default Home;
